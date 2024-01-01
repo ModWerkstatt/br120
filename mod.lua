91fb3b606dc4d00dfca407003cf518b4eaf1a1d0
Florian Thiel <flth@flth.de>
2024-01-01 16:15:20 +0100

    Releaseversion 1.1

---
+++ mod.lua
@@ -0,0 +1,28 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 2,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		authors = {
+		    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },
+		},
+		tags = { "europe", "train", "bundesbahn", "deutsche bahn", "ic", "ir", "high speed", "universal" },
+		tfnetId = { },
+		minGameVersion = 0,
+		dependencies = { },
+		url = { "" },
+	},
+	options = {
+	},
+
+	runFn = function (settings)
+
+	end
+	}
+end
---
+++ res/config/sound_set/br120.lua
@@ -0,0 +1,70 @@
+﻿local bbs2util = require "bbs2util"
+
+function data()
+return {
+	tracks = {
+		{ name = "vehicle/anfahrt.wav", refDist = 25.0 },
+		{ name = "vehicle/Lueftung.wav", refDist = 25.0 },
+		{ name = "vehicle/stand.wav", refDist = 25.0 },
+		{ name = "vehicle/kurve.wav", refDist = 25.0 },
+		{ name = "vehicle/train/brakes.wav", refDist = 25.0 },
+	},
+	events = {
+		horn = { names = { "vehicle/einheitsloks/br110_horn.wav" }, refDist = 16.0 },
+	},
+	updateFn = function (input)
+		return {
+			tracks = {
+			-- anfahrt
+				{
+				  gain = bbs2util.sampleCurve({
+							{0.0, 0.0 },
+							{0.09, 0.0 },
+							{0.1, 0.7 },
+							{0.3, 0.9 },
+							{0.4, 0.7 },
+							{0.6, 0.4 },
+							{1.0, 0.0 },
+						 }, input.speed01),
+				  pitch = bbs2util.sampleCurve({
+							{0.0, 0.9 },
+							{0.34, 0.9 },
+							{1.0, 0.9 },
+						  }, input.speed01)
+				},
+			-- Lueftung
+				{ gain = bbs2util.sampleCurve({
+							{0.0, 0.0 },
+							{0.01, 0.9 },
+							{0.33, 1.0 },
+							{1.0, 1.1 },
+						 }, input.speed01),
+				  pitch = bbs2util.sampleCurve({
+							{0.0, 0.9 },
+							{0.15, 1.0 },
+							{1.0, 1.0 },
+						  }, input.speed01)
+				},
+			-- stand
+				{
+				  gain = bbs2util.sampleCurve({
+							{0.0, 1.0 },
+							{0.01, 0.1 },
+							{0.25, 0 },
+							{1.0, 0 },
+						 }, input.speed01),
+				  pitch = bbs2util.sampleCurve({
+							{0.0, 0.7 },
+							{1.0, 0.7 },
+						  }, input.speed01)
+				},
+			    bbs2util.squeal(input.speed, input.sideForce, input.maxSideForce),
+				bbs2util.brake(input.speed, input.brakeDecel, 0.5),
+			},
+			events = {
+				horn = { gain = 1.0, pitch = 1.0 }
+			}
+		}
+	end
+	}
+end
